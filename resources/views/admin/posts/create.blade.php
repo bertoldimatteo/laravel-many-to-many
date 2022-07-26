86838a2c5f6de182174f999fc9d35575d59f6da2
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-26 17:36:42 +0200

    aggiunto name[] per checkbox

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -39,7 +39,7 @@
                         <h5>Tags</h5>
                         @foreach ($tags as $tag)
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" id="{{$tag->slug}}" value="{{$tag->id}}">
+                                <input class="form-check-input" type="checkbox" id="{{$tag->slug}}" value="{{$tag->id}}" name="tags[]">
                                 <label class="form-check-label" for="{{$tag->slug}}">{{$tag->name}}</label>
                             </div>
                         @endforeach
