67b07414ec7c8c820e225d9ff5292f4b129ac394
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-26 15:41:52 +0200

    first commit

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h1>{{$post->title}}</h1>
+                @if ($post->category)
+                    <span class="badge badge-primary">{{$post->category->name}}</span>
+                @endif
+            </div>
+            <div class="card-body">
+                <div class="mb-3">
+                    <a href="{{route('admin.posts.index')}}" class="btn btn-info">Tutti i Posts</a>
+                </div>
+                <div>
+                    {{$post->content}}
+                </div>    
+            </div>
+        </div>        
+    </div>    
+@endsection
