89ab9e04de61efdedcb9ee958b390ad2dcbb41d0
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-27 15:02:34 +0200

    aggiunto destroy al tag

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -105,8 +105,10 @@ class TagController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Tag $tag)
     {
-        //
+        $tag->delete();
+
+        return redirect()->route('admin.tags.index');
     }
 }
